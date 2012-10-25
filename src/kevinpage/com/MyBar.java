918895c7b4c8bdd3b2bde9c34c5635467d70de41
Zachary Herr <zherr@luc.edu>
2012-10-24 23:31:15 -0500

    added some comments TODO later

--- src/kevinpage/com/MyBar.java
+++ src/kevinpage/com/MyBar.java
@@ -16,7 +16,10 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.provider.BaseColumns;
 import android.util.FloatMath;
 import android.view.View;
 import android.widget.Button;
@@ -98,6 +101,7 @@ public class MyBar extends Activity {
 		
 		/**
 		 * Read in all drinks from raw file.
+		 * TODO move data reading-in to FeedReaderContract
 		 */
 		if (!data.generatedDrinks) {
 			String line;
