ac4148d700c81462a0d30c7526dd4c71061593f6
Zachary Herr <zherr@luc.edu>
2012-10-30 20:31:35 -0500

    adding loading screen for MyBar

--- src/kevinpage/com/LoadingScreen.java
+++ src/kevinpage/com/LoadingScreen.java
@@ -1,26 +1,57 @@
 package kevinpage.com;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
 
 public class LoadingScreen extends Activity {
-	
-	private ProgressBar progressBar;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_loading_screen);
-        progressBar = (ProgressBar) findViewById(R.id.progressBar1);
-        
-        
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_loading_screen, menu);
-        return true;
-    }
+	// private ProgressBar progressBar;
+	// private CountDownTimer cdt;
+	private SqlDatabase sqlDb;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_loading_screen);
+
+		sqlDb = new SqlDatabase(this);
+
+		/**
+		 * Create 6 second timer to allow database to load completely. TODO This
+		 * is probably bad practice to have arbitrary waiting time. Find a
+		 * better way to wait.
+		 */
+		new CountDownTimer(6000, 1000) {
+
+			public void onTick(long millisUntilFinished) {
+				// Log.d("WHATEVER", "TICK TICK TICK, ZACH");
+			}
+
+			public void onFinish() {
+				((Button) findViewById(R.id.enterButton))
+						.setVisibility(View.VISIBLE);
+			}
+		}.start();
+
+	}
+
+	/**
+	 * Handles button click for enter press
+	 * 
+	 * @param view
+	 *            The view of the button
+	 */
+	public void onEnter(final View view) {
+		Intent myIntent = new Intent(view.getContext(), MyBar.class);
+		LoadingScreen.this.startActivity(myIntent);
+		this.finish();
+	}
 }
