3d0fce128e38e3b241a8ec1bc9b3dc44d3a1dac4
Zachary Herr <zherr@luc.edu>
2012-11-04 23:08:50 -0600

    updated LoadingScreen with AsyncTask

--- src/kevinpage/com/LoadingScreen.java
+++ src/kevinpage/com/LoadingScreen.java
@@ -1,12 +1,15 @@
 package kevinpage.com;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class LoadingScreen extends Activity {
 
@@ -18,39 +21,25 @@ public class LoadingScreen extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_loading_screen);
+		
+		final Context context = this;
+		
+		new AsyncTask<Void, Boolean, Void>() {
 
-		sqlDb = new SqlDatabase(this);
+			@Override
+			protected Void doInBackground(Void... params) {
+				sqlDb = new SqlDatabase(context);
 
-		/**
-		 * Create 6 second timer to allow database to load completely. TODO This
-		 * is probably bad practice to have arbitrary waiting time. Find a
-		 * better way to wait.
-		 */
-		new CountDownTimer(6000, 1000) {
-
-			public void onTick(long millisUntilFinished) {
-				// Log.d("WHATEVER", "TICK TICK TICK, ZACH");
+				return null;
 			}
+			
 
-			public void onFinish() {
-				((ProgressBar) findViewById(R.id.progressBar1))
-						.setEnabled(false);
-				((Button) findViewById(R.id.enterButton))
-						.setVisibility(View.VISIBLE);
+			@Override
+			protected void onPostExecute(Void result){
+				Intent myIntent = new Intent(context, MyBar.class);
+				LoadingScreen.this.startActivity(myIntent);
+				finish();
 			}
-		}.start();
-
-	}
-
-	/**
-	 * Handles button click for enter press
-	 * 
-	 * @param view
-	 *            The view of the button
-	 */
-	public void onEnter(final View view) {
-		Intent myIntent = new Intent(view.getContext(), MyBar.class);
-		LoadingScreen.this.startActivity(myIntent);
-		this.finish();
+		}.execute();
 	}
 }
